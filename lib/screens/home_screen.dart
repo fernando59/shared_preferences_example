d8f298c29fa618baab74053ba24362fe8f19393e
Fernando Mercado <fernandomercado529@gmail.com>
2023-01-12 09:38:26 -0400

    Add sidebar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/sidemenu_widget.dart';
+
 class HomeScreen extends StatelessWidget {
   static const String routerName = 'home';
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: const Text(
-            'Home Screen',
-          ),
-          centerTitle: true),
-      body: const Center(
-        child: Text('HomeScreen'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+            title: const Text(
+              'Home Screen',
+            ),
+            centerTitle: true),
+        drawer: const SideMenuWidget(),
+        body: const Center(
+          child: Text('HomeScreen'),
+        ),
       ),
     );
   }
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/sidemenu_widget.dart';
+
 class SettingsScreen extends StatelessWidget {
   static const String routerName = 'settings';
   const SettingsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Seetings '),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text('SettingsScreen'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Seetings '),
+          centerTitle: true,
+        ),
+        drawer: const SideMenuWidget(),
+        body: const Center(
+          child: Text('SettingsScreen'),
+        ),
       ),
     );
   }
